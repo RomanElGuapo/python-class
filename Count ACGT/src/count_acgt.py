3998ca9f6538c136953baac4257c1a5a7ecfe6c3
RomanElGuapo <160921322+RomanElGuapo@users.noreply.github.com>
2024-04-22 20:20:21 -0600

    Se añadió el argumento posicional del nombre del archivo de texto de la secuencia y la opción -n seguida de A,C,G, o T para mostrar la cantidad de un nucleótido en la secuencia

--- Count ACGT/src/count_acgt.py
+++ Count ACGT/src/count_acgt.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 '''
 NAME: Conteo de Bases de ADN
 
-VERSION: 1.0
+VERSION: 2.0
 
 AUTHOR: Ángel Román Zamora López
 
@@ -31,44 +33,69 @@ SEE ALSO:
 # =                            imports
 # ===========================================================================
 
-
-
-
+import argparse
 
 # ===========================================================================
 # =                            Command Line Options
 # ===========================================================================
 
-
-
-
-
+# No se requieren opciones de línea de comandos adicionales.
 
 # ===========================================================================
 # =                            functions
 # ===========================================================================
 
-
-
-
+# No se requieren funciones adicionales.
 
 # ===========================================================================
 # =                            main
 # ===========================================================================
-cadena_ADN = input('Inserte el nombre de su archivo a continuación: ')
 
-A=C=G=T = 0
+# Se define un analizador de argumentos para manejar las opciones de línea de comandos.
+parser = argparse.ArgumentParser(description="Lee archivo de entrada y salida")
 
+# Se agrega un argumento posicional para el archivo de entrada.
+parser.add_argument("input_file", type=str, help="El archivo de texto que quieres procesar.")
+
+# Se agrega un argumento opcional para especificar el nucleótido cuyo conteo se desea obtener.
+parser.add_argument('-n', choices=['A', 'C', 'T','G'],
+                    help='Elige tu nucleotido')
+
+# Se analizan los argumentos de la línea de comandos.
+args = parser.parse_args()
+
+# Se guarda la ruta del archivo de ADN en una variable.
+cadena_ADN = args.input_file
+
+# Se inicializan contadores para cada base de ADN.
+A = C = G = T = 0
+
+# Se abre el archivo de ADN para su procesamiento.
 with open(cadena_ADN) as secuencia:
-  for linea in secuencia:
-    for letra in linea:
-      if letra.upper() == 'A':
-        A += 1
-      if letra.upper() == 'C': 
-        C += 1
-      if letra.upper() == 'T':
-        T += 1 
-      if letra.upper() == 'G':
-        G += 1
+    # Se itera sobre cada línea del archivo.
+    for linea in secuencia:
+        # Se itera sobre cada letra en la línea.
+        for letra in linea:
+            # Se incrementa el contador correspondiente a la base de ADN encontrada.
+            if letra.upper() == 'A':
+                A += 1
+            if letra.upper() == 'C': 
+                C += 1
+            if letra.upper() == 'T':
+                T += 1 
+            if letra.upper() == 'G':
+                G += 1
 
-print(f'Cantidad de A:{A}, C:{C}, T:{T}, G:{G}')
+# Se imprime el conteo de la base de ADN especificada por el usuario, si se proporcionó.
+if args.n == 'A':
+    print('Cantidad de A:', A)
+if args.n == 'C':
+    print('Cantidad de C:', C)
+if args.n == 'T':
+    print('Cantidad de T:', T)
+if args.n == 'G':
+    print('Cantidad de G:', G)
+
+# Si no se especifica ninguna base de ADN, se imprime el conteo de todas las bases.
+if not args.n:
+    print('Cantidad de A:', A, 'C:', C, 'T:', T, 'G:', G)
